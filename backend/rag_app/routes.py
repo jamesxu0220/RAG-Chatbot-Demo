afa5ea002049f1bf2b006a4a7d61f8516d37903f
James Xu <zx2370@columbia.edu>
2025-03-30 20:06:16 -0400

    fix: Fix name of blueprint

--- backend/rag_app/routes.py
+++ backend/rag_app/routes.py
@@ -2,9 +2,10 @@
 
 from flask import Blueprint, jsonify
 
-base = Blueprint("", __name__)
+base = Blueprint("base", __name__)
 
 
 @base.route("/status", methods=["GET"])
 def status():
+    """Return the status of the application."""
     return jsonify({"status": "NORMAL"}), 200
